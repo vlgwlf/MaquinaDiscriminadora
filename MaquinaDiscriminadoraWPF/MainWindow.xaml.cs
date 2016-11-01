310f089d6d00186625edf78d7b23ed2b3ba5472d
vlgwlf <vitakub64@gmail.com>
2016-11-01 13:39:53 -0700

    Added basic logic to new class.

--- MaquinaDiscriminadoraWPF/MainWindow.xaml.cs
+++ MaquinaDiscriminadoraWPF/MainWindow.xaml.cs
@@ -25,43 +25,57 @@ namespace MaquinaDiscriminadoraWPF
         string[] reserved = new string[] { "box","open","flip","keep","label","fold","otherwise"};
         string[] operators = new string[] { "+", "-", "*", "/", "AND", "OR", "and", "or", "==", "<", ">", ">=", "<="};
         string[] agrupadores = new string[] { "(", "[", "{", ")", "]", "}" };
-
+        bool notResFlag, notOpFlag, notGrFlag, notDecFlag, notTerFlag;
         string declarativo = "<-";
         Microsoft.Win32.OpenFileDialog file;
+        List<string> palabras = new List<string>();
+        List<int> tokens = new List<int>();
         StreamReader reader;
         string fileName;
         string boxesCode;
-
+        MaquinaSintactica ms;
         public MainWindow()
         {
             InitializeComponent();
         }
-
+        
         private void btn_LoadFile_Click(object sender, RoutedEventArgs e)
         {
+            input_Block.Text = "";
+            output_Box.Text = "";
             file = new Microsoft.Win32.OpenFileDialog();
             file.ShowDialog();
             fileName = file.FileName;
             if (fileName != null) { reader = new StreamReader(fileName); }
             input_Block.Text = reader.ReadToEnd();
+            
             //MessageBox.Show(fileName); /*Uncomment for debug purposes.*/
         }
 
         private void btn_Translate_Click(object sender, RoutedEventArgs e)
         {
-            string line, toLexeme;
+            string line;
             reader = new StreamReader(fileName);
-           
+            int lineCount = 0;
 
             while ((line = reader.ReadLine()) != null)
             {
                 translate(line);
+                lineCount++;
             }
+
+            notResFlag = false;
+            notOpFlag = false;
+            notTerFlag = false;
+            notGrFlag = false;
+            notDecFlag = false;
             /*
             line = reader.ReadLine();
             MessageBox.Show(line);
             LINES USED FOR DEBUGGING
-            */            
+            */
+            MessageBox.Show("Número de líneas: " + lineCount);
+            ms = new MaquinaSintactica(tokens, palabras, lineCount);
         }
 
         private void translate(string line)
@@ -69,10 +83,10 @@ namespace MaquinaDiscriminadoraWPF
             string translation = "";
             StringBuilder word = new StringBuilder();
 
-            for (int i = 0; i < line.Length-1; i++)
+            for (int i = 0; i < line.Length - 1; i++)
             {
                 word.Append(line[i]);
-                if (line[i + 1] == ' ' || line[i + 1] == '\n')
+                if (line[i + 1] == ' ' || line[i + 1] == '\n' || i == line.Length-1)
                 {
                     //revisa si es palabra reservada                 
                     foreach (string w in reserved)
@@ -81,7 +95,14 @@ namespace MaquinaDiscriminadoraWPF
                         {
                             translation = "[3, reservada]";
                             output_Box.Text += translation;
+                            tokens.Add(3);
+                            palabras.Add(word.ToString());
+                            translation = String.Empty;
                             word.Clear();
+                        }
+                        else
+                        {
+                            notResFlag = true;
                         }
                     }
                     //revisa si es operador
@@ -91,7 +112,14 @@ namespace MaquinaDiscriminadoraWPF
                         {
                             translation = "[4, operador]";
                             output_Box.Text += translation;
+                            tokens.Add(4);
+                            palabras.Add(word.ToString());
+                            translation = String.Empty;
                             word.Clear();
+                        }
+                        else
+                        {
+                            notOpFlag = true;
                         }
                     }
                     //revisa por agrupadores
@@ -101,7 +129,14 @@ namespace MaquinaDiscriminadoraWPF
                         {
                             translation = "[2, agrupador]";
                             output_Box.Text += translation;
+                            tokens.Add(2);
+                            palabras.Add(word.ToString());
+                            translation = String.Empty;
                             word.Clear();
+                        }
+                        else
+                        {
+                            notGrFlag = true;
                         }
                     }
                     //revisa si es declarativo
@@ -109,25 +144,46 @@ namespace MaquinaDiscriminadoraWPF
                     {
                         translation = "[1, declarativo]";
                         output_Box.Text += translation;
+                        tokens.Add(1);
+                        palabras.Add(word.ToString());
+                        translation = String.Empty;
                         word.Clear();
                     }
                     else
                     {
-                        translation = "[5, identificador]";
+                        notDecFlag = true;
+                    }
+                    if (line[i] == '&')
+                    {
+                        translation = "[7, terminador]";
                         output_Box.Text += translation;
+                        tokens.Add(7);
+                        palabras.Add(word.ToString());
+                        translation = String.Empty;
                         word.Clear();
                     }
+                    else
+                    {
+                        notTerFlag = true;
+                    }
+                    
                 }//if statement for space terminator
                 else if (line[i] == ' ' || line[i] == '#')
                 {
                     translation = "[6, omision]";
                     output_Box.Text += translation;
+                    tokens.Add(6);
+                    palabras.Add(word.ToString());
+                    translation = String.Empty;
                     word.Clear();
                 }
-                else if (line[i] == '&')
+                else if(notDecFlag && notGrFlag && notTerFlag && notOpFlag && notResFlag)
                 {
-                    translation = "[7, terminal]";
+                    translation = "[5, identificador]";
                     output_Box.Text += translation;
+                    tokens.Add(5);
+                    palabras.Add(word.ToString());
+                    translation = String.Empty;
                     word.Clear();
                 }
             }
---
+++ MaquinaDiscriminadoraWPF/MaquinaSintactica.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows;
+
+namespace MaquinaDiscriminadoraWPF
+{
+    class MaquinaSintactica
+    {
+        public MaquinaSintactica(List<int> tokens,List<string> palabras, int lineCount)
+        {
+            List<List<string>> parseTrees = new List<List<string>>();
+            for (int i = 0; i < tokens.Count - 1; i++)
+            {
+                if (i == 0)
+                {
+                    switch (tokens[i])//checks for identifier or reserved word
+                    {
+                        case 1:
+                            continue;
+                        case 3:
+                            continue;
+                        default:
+                            MessageBox.Show("Error in line: "+lineCount);
+                            break;
+                    }
+
+                }
+
+                if (i != ' ')//checks for spaces
+                {
+                    MessageBox.Show("Error in line: "+lineCount);
+                    break;
+                }
+
+            }//for loop bracket
+        }
+    }
+}
